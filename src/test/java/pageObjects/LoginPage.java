c424a4725d520d87b2578a59c334cc204f22d859
Gayatri-yadav-repo <gayatriyadaav6@gmail.com>
2024-02-04 15:38:40 +0530

    Base class with first TC_01

---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,39 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        driver=driver;
+        PageFactory.initElements(driver, this);
+
+    }
+
+    @FindBy (name="user-name")
+    WebElement txtusername;
+    @FindBy (name="password")
+    WebElement txtpassword;
+    @FindBy (id="login-button")
+    WebElement btn;
+
+    //encapsulation
+    public void setUsername(String uname)
+    {
+        txtusername.sendKeys(uname);
+
+    }
+    public void setPassword(String upassword)
+    {
+        txtpassword.sendKeys(upassword);
+
+    }
+public void clicksubmit(){
+        btn.click();
+}
+
+}
